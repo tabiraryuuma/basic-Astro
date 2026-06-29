d4bea125c7614454bc17895d49e18fbd1ed61109
ryuuma <ryumabasic@gmail.com>
2026-06-29 11:12:14 +0900

    インポートを自動化

---
+++ src/assets/image/Itoshima808/index.ts
@@ -0,0 +1,19 @@
+// ⚠️ このファイルは自動生成されています。
+// 手動で編集しないでください。
+
+// Images
+export { default as Itoshima808Day1 } from "./Itoshima808-day-1.webp";
+export { default as Itoshima808Day2 } from "./Itoshima808-day-2.webp";
+export { default as Itoshima808Day3 } from "./Itoshima808-day-3.webp";
+export { default as Itoshima808Day4 } from "./Itoshima808-day-4.webp";
+export { default as Itoshima808Day5 } from "./Itoshima808-day-5.webp";
+export { default as Itoshima808Map1 } from "./Itoshima808-map1.webp";
+export { default as Itoshima808Map2 } from "./Itoshima808-map2.webp";
+export { default as Itoshima808MovieLong } from "./Itoshima808-movie-long.webp";
+export { default as Itoshima808MovieLongsp } from "./Itoshima808-movie-longsp.webp";
+export { default as Itoshima808MovieShort } from "./Itoshima808-movie-short.webp";
+export { default as Itoshima808MovieShortsp } from "./Itoshima808-movie-shortsp.webp";
+export { default as Itoshima808Night1 } from "./Itoshima808-night-1.webp";
+export { default as Itoshima808Night2 } from "./Itoshima808-night-2.webp";
+export { default as Itoshima808Night3 } from "./Itoshima808-night-3.webp";
+export { default as Itoshima808Night4 } from "./Itoshima808-night-4.webp";
---
+++ src/assets/image/top/index.ts
@@ -0,0 +1,33 @@
+// ⚠️ このファイルは自動生成されています。
+// 手動で編集しないでください。
+
+// SVG
+export { default as Designoffice } from "./designoffice.svg";
+
+// Images
+export { default as 1Mainvisual2 } from "./1-mainvisual-2.webp";
+export { default as 1Mainvisual3 } from "./1-mainvisual-3.webp";
+export { default as 1Project1 } from "./1-project-1.webp";
+export { default as 1Project2 } from "./1-project-2.webp";
+export { default as 2About } from "./2-about.webp";
+export { default as 2Slide1 } from "./2-slide-1.webp";
+export { default as 2Slide2 } from "./2-slide-2.webp";
+export { default as 2Slide3 } from "./2-slide-3.webp";
+export { default as 2Slide4 } from "./2-slide-4.webp";
+export { default as 2Slide5 } from "./2-slide-5.webp";
+export { default as 2Slide6 } from "./2-slide-6.webp";
+export { default as 2Slide7 } from "./2-slide-7.webp";
+export { default as 2Slide8 } from "./2-slide-8.webp";
+export { default as Top1Sp } from "./top-1-sp.webp";
+export { default as TopMainvisual1 } from "./top-mainvisual-1.webp";
+export { default as TopMainvisual1sp } from "./top-mainvisual-1sp.webp";
+export { default as TopMainvisual2 } from "./top-mainvisual-2.webp";
+export { default as TopMainvisual2sp } from "./top-mainvisual-2sp.webp";
+export { default as TopMainvisual3 } from "./top-mainvisual-3.webp";
+export { default as TopMainvisual3sp } from "./top-mainvisual-3sp.webp";
+export { default as TopMainvisual4 } from "./top-mainvisual-4.webp";
+export { default as TopMainvisual4sp } from "./top-mainvisual-4sp.webp";
+export { default as TopMainvisual5 } from "./top-mainvisual-5.webp";
+export { default as TopMainvisual5sp } from "./top-mainvisual-5sp.webp";
+export { default as TopMainvisual6 } from "./top-mainvisual-6.webp";
+export { default as TopMainvisual6sp } from "./top-mainvisual-6sp.webp";
